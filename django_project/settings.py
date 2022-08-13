48710f6e28cfc3f90c5ea3ee20daaddd022308f1
sachinraghult <sachinraghult2002@gmail.com>
2022-08-13 12:05:19 +0530

    ADD: login and logout system

--- django_project/settings.py
+++ django_project/settings.py
@@ -120,4 +120,8 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
+# Modifying settings
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+LOGIN_REDIRECT_URL = "blog-home"
+LOGIN_URL = "login"
